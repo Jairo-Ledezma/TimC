48fa355361ddb6a1aa3c9f82e4feec2ca13317d5
Jairo Ledezma <jairo.arturo.ledezma@gmail.com>
2023-02-28 15:09:36 -0600

    added while and do while class

---
+++ src/S6ControlFlow/WhileAndDoWhileLoop.java
@@ -0,0 +1,35 @@
+
+package S6ControlFlow;
+
+public class WhileAndDoWhileLoop {
+
+	public static void main(String[] args) {
+
+		
+		// for loop
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+
+		
+		// while loop (note that the index is declared outside of the loop)
+		int i = 1;
+		while (true) {
+			if (i > 5) {
+				break;
+			}
+			System.out.println(i);
+			i++;
+		}
+		
+		//do while loop (index also declared outside this will run at least once since condition)
+		//is checked in the end
+		int x=1;
+		do {
+			System.out.println(x);
+			x++;
+		}while(x<=5);
+
+	}
+
+}
