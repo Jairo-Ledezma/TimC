d957b05ec799ed1ae347e3751492bad7a29532ef
Jairo Ledezma <jairo.arturo.ledezma@gmail.com>
2023-02-28 19:26:25 -0600

    added a class explaining primitive data types and the use of wrpper classes as well as autoboxing and unboxing

---
+++ src/S5expressionsStatements/PrimitiveDataTypes.java
@@ -0,0 +1,34 @@
+package S5expressionsStatements;
+
+public class PrimitiveDataTypes {
+	static boolean isboolean;
+	static byte bait;
+	static char chara;
+	static short shory;
+	static int num = 1;
+	static long bnum;
+	static float deci;
+	static double bdeci;
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(isboolean);
+		System.out.println(bait);
+		System.out.println(chara);
+		System.out.println(shory);
+		System.out.println(num);
+		System.out.println(bnum);
+		System.out.println(deci);
+		System.out.println(bdeci);
+		String num10 = "ten";
+		
+		Integer num1 = 10; // autoboxing, the value assigned to num 1 is a primitive value but the variable num1 is an instance of the class Integer 
+		
+		
+		
+		
+
+	}
+
+}
