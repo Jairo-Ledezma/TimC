7baa9dc74e492d74928ceb9f530f0cc6cc0fddbe
Jairo Ledezma <jairo.arturo.ledezma@gmail.com>
2023-03-06 18:41:01 -0600

    added Pokeball class and Pokemon classes to it

---
+++ src/S8Polymorphism/PokeballMain.java
@@ -0,0 +1,12 @@
+package S8Polymorphism;
+
+public class PokeballMain {
+
+	public static void main(String[] args) {
+		PokeBall pokeball = new PokeBall();
+		
+		pokeball.choosePokemon(true, false, false, true, false, true);
+
+	}
+
+}
